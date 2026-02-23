263be87888b57e513b1037173a9631a750bcde6e
prjctlphbt <prjctlphbt@gmail.com>
2026-02-23 16:58:34 -0500

    [Orbitr] Install meta pixel (99040120260223215832)

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,22 @@ fbq('track', 'PageView');
 src="https://www.facebook.com/tr?id=990301215537&ev=PageView&noscript=1"
 /></noscript>
 {/* <!-- End Orbitr Pixel: meta:990301215537 --> */}
+{/* <!-- Orbitr Pixel: meta:99040120260223215832 --> */}
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '99040120260223215832');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=99040120260223215832&ev=PageView&noscript=1"
+/></noscript>
+{/* <!-- End Orbitr Pixel: meta:99040120260223215832 --> */}
 </body>
     </html>
   );
