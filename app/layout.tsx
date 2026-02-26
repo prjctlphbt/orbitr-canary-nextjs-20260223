ff4b8dab659d43d4db4d70d4bfdb2056c07705a0
prjctlphbt <prjctlphbt@gmail.com>
2026-02-26 12:46:21 -0500

    [Orbitr] Install meta pixel (1234567890)

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,6 +81,21 @@ fbq('track', 'PageView');
 src="https://www.facebook.com/tr?id=990001214036&ev=PageView&noscript=1"
 /></noscript>
 {/* <!-- End Orbitr Pixel: meta:990001214036 --> */}
+{/* <!-- Orbitr Pixel: meta:1234567890 --> */}
+<script
+  dangerouslySetInnerHTML={{
+    __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1234567890');
+fbq('track', 'PageView');`,
+  }}
+/>
+{/* <!-- End Orbitr Pixel: meta:1234567890 --> */}
 </body>
     </html>
   );
